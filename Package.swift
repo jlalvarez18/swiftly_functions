8b445e2f75530ac058aab1fc30c0117bbcba4e4c
Juan Alvarez <jlalvarez18@gmail.com>
2017-09-24 15:43:04 -0500

    added SwiftlyFunctionController protocol

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftly_functions",
+    name: "SwiftlyFunctions",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,14 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swiftly_functions",
-            dependencies: ["PromiseKit"]),
+            dependencies: ["Vapor"]),
         .testTarget(
             name: "swiftly_functionsTests",
             dependencies: ["swiftly_functions"]),
--- Sources/swiftly_functions/swiftly_functions.swift
+++ Sources/swiftly_functions/swiftly_functions.swift
@@ -1,3 +1,36 @@
-struct swiftly_functions {
-    var text = "Hello, World!"
+
+import Foundation
+import Vapor
+
+public protocol SwiftlyFunctionController {
+    static func defineFunctions(context: Swiftly)
+}
+
+public class Swiftly {
+    
+    private var functions: [String: RequestResponseBlock] = [:]
+    
+    private init() {}
+    
+    public typealias RequestResponseBlock = (Request) throws -> ResponseRepresentable
+    
+    public static let shared: Swiftly = Swiftly()
+    
+    public func define(_ name: String, block: @escaping RequestResponseBlock) {
+        self.functions[name] = block
+    }
+    
+    public func performFunction(_ request: Request) throws -> ResponseRepresentable {
+        let name = try request.parameters.next(String.self)
+        
+        guard let function = self.functions[name] else {
+            throw Abort.badRequest
+        }
+        
+        return try function(request)
+    }
+    
+    public func register(controller: SwiftlyFunctionController.Type) {
+        controller.defineFunctions(context: self)
+    }
 }
