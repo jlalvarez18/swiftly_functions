78757aa65883986fa6db87b3d585bc7dc75aff45
Juan Alvarez <jlalvarez18@gmail.com>
2017-09-29 10:20:04 -0500

    Swiftly is no longer a singleton, added run() to start server, added GET/POST functions endpoint

--- Sources/swiftly_functions/swiftly_functions.swift
+++ Sources/swiftly_functions/swiftly_functions.swift
@@ -10,11 +10,7 @@ public class Swiftly {
     
     private var functions: [String: RequestResponseBlock] = [:]
     
-    private init() {}
-    
     public typealias RequestResponseBlock = (Request) throws -> ResponseRepresentable
-    
-    public static let shared: Swiftly = Swiftly()
     
     public func define(_ name: String, block: @escaping RequestResponseBlock) {
         self.functions[name] = block
@@ -32,5 +28,23 @@ public class Swiftly {
     
     public func register(controller: SwiftlyFunctionController.Type) {
         controller.defineFunctions(context: self)
+    }
+    
+    public func run() throws {
+        let droplet = try Droplet()
+        
+        droplet.post("functions", String.parameter) { (req) -> ResponseRepresentable in
+            return try self.performFunction(req)
+        }
+        
+        droplet.get("functions") { (req) -> ResponseRepresentable in
+            var json = JSON()
+            
+            try json.set("functions", Array(self.functions.keys))
+            
+            return json
+        }
+        
+        try droplet.run()
     }
 }
